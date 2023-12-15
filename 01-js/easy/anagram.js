fc9a46ddf2da3f911c79b5fb85b99a468e70ba08
AshishV306 <110739725+AshishV306@users.noreply.github.com>
2023-12-15 20:20:25 +0530

    easy week 1 solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
+  // Check if the lengths are the same, if not, they can't be anagrams
+  if (cleanStr1.length !== cleanStr2.length) {
+    return false;
+  }
+
+  // Create hashmaps to store character frequencies
+  const charCount1 = {};
+  const charCount2 = {};
+
+  // Count character occurrences in the first string
+  for (const char of cleanStr1) {
+    charCount1[char] = (charCount1[char] || 0) + 1;
+  }
+
+  // Count character occurrences in the second string
+  for (const char of cleanStr2) {
+    charCount2[char] = (charCount2[char] || 0) + 1;
+  }
+
+  // Compare the character frequencies in both hashmaps
+  for (const char in charCount1) {
+    if (charCount1[char] !== charCount2[char]) {
+      return false;
+    }
+  }
+
+  // If all frequencies match, the strings are anagrams
+  return true;
 }
 
 module.exports = isAnagram;
